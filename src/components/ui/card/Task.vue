173a09324526c6735e2ee8bd3f96b05f33cbf09f
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-11-13 12:31:15 +0300

    ss

---
+++ src/components/ui/card/Task.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="task">
+    <div class="task_head">
+      <p>Создание видео-курса</p>
+      <div class="task_head__more">
+        <Icons icon="pepicons-pencil:dots-x" />
+      </div>
+    </div>
+    <div class="task_body">Срок: 5 дней</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.task {
+  background-color: $light;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.task_head {
+  @include flex-space;
+  gap: 20px;
+}
+</style>
