970dbb6f273a54460b51e0c10ff54fa35a2447b2
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-11-16 15:52:38 +0300

    ыы

--- src/stores/useUsersStore.ts
+++ src/stores/useUsersStore.ts
@@ -1,22 +1,16 @@
-<<<<<<< HEAD:src/store/useUserStore.ts
 import { defineStore, storeToRefs } from "pinia";
+import axios from "axios";
 import { api } from "@/api/api";
-import { useRouter } from "vue-router";
-=======
-import { defineStore, storeToRefs } from 'pinia';
-import axios from 'axios';
-import { api } from '@/api/api';
->>>>>>> 59468546baa606473be359619aac9018a2dd2e6d:src/stores/useUsersStore.ts
 
 export const useUsersStore = defineStore("user", {
   state: () => ({
     users: null as any,
-    auth: null as string | null
+    auth: null as string | null,
   }),
   actions: {
     async signIn(user: any) {
       try {
-        await api.post("/wp-json/wp/v2/register/user", user, {
+        await axios.post("/wp-json/wp/v2/register/user", user, {
           headers: {
             "Content-Type": "application/json",
           },
@@ -26,45 +20,20 @@ export const useUsersStore = defineStore("user", {
       }
     },
     async login(user: any) {
-<<<<<<< HEAD:src/store/useUserStore.ts
-      const router = useRouter();
+      // const router = useRouter();
       try {
-        const response = await api.post("/wp-json/api/v1/token", user, {
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-=======
-      // const router = useRouter(); 
-      try {
-        const response = await api.post('/wp-json/api/v1/token', user, {
->>>>>>> 59468546baa606473be359619aac9018a2dd2e6d:src/stores/useUsersStore.ts
-        });
-        this.auth = response.data
+        const response = await api.post("/wp-json/api/v1/token", user, {});
+        this.auth = response.data;
 
-<<<<<<< HEAD:src/store/useUserStore.ts
-        await router.push("/clients?page=1&view=list&count=10");
-=======
         // await router.push('/clients?page=1&view=list&count=10');
->>>>>>> 59468546baa606473be359619aac9018a2dd2e6d:src/stores/useUsersStore.ts
       } catch (error) {
         console.log("Login error:", error);
       }
     },
     async refreshToken() {
       try {
-<<<<<<< HEAD:src/store/useUserStore.ts
-        const response = await api.post(
-          "/wp-json/jwt-auth/v1/token/validate",
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${this.users.token}`,
-            },
-          }
-        );
+        const response = await api.get("/wp-json/api/v1/token-validate");
         console.log(response.data);
-=======
-        const response = await api.get('/wp-json/api/v1/token-validate');
-        console.log(response.data)
->>>>>>> 59468546baa606473be359619aac9018a2dd2e6d:src/stores/useUsersStore.ts
       } catch (error) {
         console.log("Refresh token error:", error);
       }
@@ -77,40 +46,19 @@ export const useUsersStore = defineStore("user", {
       this.$reset();
       localStorage.removeItem("user");
     },
-    // async fetchUserInfo() {
-    //   if (this.users) {
-    //     await this.refreshToken();
-    //     try {
-    //       const response = await axios.get("/wp-json/wp/v2/users/me", {
-    //         headers: {
-    //           Authorization: `Bearer ${this.users.token}`,
-    //         },
-    //       });
-    //       this.setUser({ ...this.users, userInfo: response.data });
-    //     } catch (error) {
-    //       console.error("Failed to fetch user info:", error);
-    //     }
-    //   }
-    // },
-
-<<<<<<< HEAD:src/store/useUserStore.ts
-    loadUserFromLocalStorage() {
-      const userData = localStorage.getItem("user");
-      if (userData) {
-        this.users = JSON.parse(userData);
-=======
-        await this.refreshToken()
+    async fetchUserInfo() {
+      if (this.users) {
+        await this.refreshToken();
         try {
-          const response = await axios.get('/wp-json/wp/v2/users/me', {
+          const response = await axios.get("/wp-json/wp/v2/users/me", {
             headers: {
               Authorization: `Bearer ${this.users.token}`,
             },
           });
           this.setUser({ ...this.users, userInfo: response.data });
         } catch (error) {
-          console.error('Failed to fetch user info:', error);
+          console.error("Failed to fetch user info:", error);
         }
->>>>>>> 59468546baa606473be359619aac9018a2dd2e6d:src/stores/useUsersStore.ts
       }
     },
   },
